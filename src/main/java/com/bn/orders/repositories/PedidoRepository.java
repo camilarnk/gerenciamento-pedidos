aab8d73a0180c687015d485fbcf77e7965473695
Camila Reinke <reinkecamila@gmail.com>
2026-03-18 15:10:06 -0300

    feat: adiciona repositório JPA

---
+++ src/main/java/com/bn/orders/repositories/PedidoRepository.java
@@ -0,0 +1,8 @@
+
+package com.bn.orders.repositories;
+
+import com.bn.orders.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
